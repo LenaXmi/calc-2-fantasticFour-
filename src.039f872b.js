816fb81dbf663d00709ba089ee233116a2f0f707
LenaXmi <81982732+LenaXmi@users.noreply.github.com>
2021-07-09 10:33:13 +0000

    Deploying to gh-pages from @ LenaXmi/calc-2-fantasticFour-@58161b5aeda50b34c2fc69eba9b88d2777a5d8d1 🚀

--- src.039f872b.js
+++ src.039f872b.js
@@ -7,4 +7,4 @@ $("a.scrollto").click(function(){var t=$(this).attr("href"),o=$(t).offset().top;
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/mobile-menu"),require("./js/anchor");
 },{"./sass/main.scss":"clu1","./js/mobile-menu":"sByI","./js/anchor":"v66i"}]},{},["Focm"], null)
-//# sourceMappingURL=/calc-2-fantasticFour-/src.fe169767.js.map
+//# sourceMappingURL=/calc-2-fantasticFour-/src.039f872b.js.map
